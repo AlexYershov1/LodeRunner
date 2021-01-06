292b823b287c95a24bdf955cf9c6a8a7aa6ad979
AlexYershov1 <alexyershov1@gmail.com>
2021-01-06 20:55:40 +0200

    c-tor updated

--- Project_Anael_Zateikin_Alexander_Yershov/include/Enemy.h
+++ Project_Anael_Zateikin_Alexander_Yershov/include/Enemy.h
@@ -7,12 +7,14 @@ class Enemy : public MovingObject
 public:
 	Enemy(const sf::Texture*, const sf::Vector2f&, int, int, std::vector<sf::Texture*>&);
 	~Enemy();
-	void move(sf::Time&) override;
+
+	virtual void move(sf::Time&) override;
+
 	virtual void handleCollision(GameObject&);
 	virtual void handleCollision(Wall&);
 	virtual void handleCollision(Player&);
 	virtual void handleCollision(Enemy&) override {};
-	virtual void handleCollision(Bar&) override {/*do stuff*/};
+	virtual void handleCollision(Bar&) override;
 	virtual void handleCollision(Ladder&);
 	virtual void handleCollision(Coin&) override {};
 	virtual void handleCollision(Bonus&) override {};
