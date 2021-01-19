238b933432fabd30f3c4e1db3814521c219107ab
anael <anaelza@hac.ac.il>
2021-01-19 23:25:56 +0200

    deleted static member

--- Project_Anael1_Zateikin1_Alexander2_Yershov2/include/staticInclude/Coin.h
+++ Project_Anael1_Zateikin1_Alexander2_Yershov2/include/staticInclude/Coin.h
@@ -4,13 +4,12 @@
 class Coin : public StaticObject
 {
 public:
-	//using::StaticObject::StaticObject;
 	Coin(Elements, const sf::Vector2f&, int, int);
 	~Coin();
 	virtual void handleCollision(Player&, Controller&) override;
 	virtual void handleCollision(Enemy&, Controller&) override {};
 
-	static bool checkCoinsStatus() ; //returns true if at least one coin still exists
+	//static bool checkCoinsStatus() ; //returns true if at least one coin still exists
 private:
-	static int coinsLeft;
+	//static int coinsLeft;
 };
